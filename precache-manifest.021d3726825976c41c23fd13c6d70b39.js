64b622ec8643bf8a2eeb28f0d75565826d38096d
Kamil Balogun <kamilbalogun1@gmail.com>
2020-04-29 13:02:19 +0100

    Updates

---
+++ precache-manifest.021d3726825976c41c23fd13c6d70b39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7949549f6873ca57ef124d1bd990bbf",
+    "url": "/covid-19-estimator-app-deploy/index.html"
+  },
+  {
+    "revision": "8c7a801c6eed4533bfb5",
+    "url": "/covid-19-estimator-app-deploy/static/css/2.8f8036f3.chunk.css"
+  },
+  {
+    "revision": "81bff45553d080fb1c43",
+    "url": "/covid-19-estimator-app-deploy/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8c7a801c6eed4533bfb5",
+    "url": "/covid-19-estimator-app-deploy/static/js/2.41a99d05.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid-19-estimator-app-deploy/static/js/2.41a99d05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81bff45553d080fb1c43",
+    "url": "/covid-19-estimator-app-deploy/static/js/main.2a1146e6.chunk.js"
+  },
+  {
+    "revision": "a820de66fa008fc9bd53",
+    "url": "/covid-19-estimator-app-deploy/static/js/runtime-main.f9f83248.js"
+  }
+]);
